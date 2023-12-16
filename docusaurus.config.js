aace15ab7c031aa0c3499fd1666a6160866d3d4c
k <hoge@example.com>
2023-12-16 11:43:49 +0900

    config修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,11 +16,11 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/law-documents/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '', // Usually your GitHub org/user name.
+  organizationName: 'hogehoge39292', // Usually your GitHub org/user name.
   projectName: 'law-documents', // Usually your repo name.
 
   onBrokenLinks: 'throw',
